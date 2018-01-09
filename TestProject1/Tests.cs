3c3e81aa3ae73a29e4a9a169c2873fbe6590a8bd
Nicolas Dextraze <nicolas.dextraze@dexdo.com>
2018-01-09 06:59:14 -0800

    Adding first test

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace TestProject1
@@ -7,9 +9,69 @@ namespace TestProject1
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void CancelAnOrder()
         {
-            Assert.True(true);
+            //Given
+            var order = new Order();
+            order.Hydrate(new OrderReceived());
+            
+            //When
+            var events = order.Execute(new CancelOrder());
+            
+            //Then
+            Assert.True(events.Count() == 1);
+            Assert.True(events.First() is OrderCanceled);
         }
+    }
+
+    public class OrderCanceled
+    {
+        
+    }
+
+    public class OrderReceived
+    {
+        
+    }
+
+    public class Order
+    {
+        private bool received;
+
+        public IEnumerable<object> Execute(object order)
+        {
+            if (order is CancelOrder)
+            {
+                return CancelOrder((CancelOrder) order);
+            }
+            throw new InvalidOperationException("Unknown command.");
+        }
+
+        private IEnumerable<object> CancelOrder(CancelOrder cancelOrder)
+        {
+            if (!received)
+            {
+                throw new Exception("Order not received.");
+            }
+
+            return new[] {new OrderCanceled()};
+        }
+
+        public void Hydrate(object @event)
+        {
+            if (@event is OrderReceived)
+            {
+                OnOrderReceived((OrderReceived) @event);
+            }
+        }
+
+        private void OnOrderReceived(OrderReceived @event)
+        {
+            this.received = true;
+        }
+    }
+
+    public class CancelOrder
+    {
     }
 }
