1e5198271e5d6ffa7d4b8c5a27a8c1d07f6d494e
patrick.lafrance <patrick.lafrance@outlook.com>
2018-01-09 10:08:45 -0500

    Added the UI for the CancelOrder screen

---
+++ Web/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public ActionResult CancelOrder()
+        {
+            return this.View("CancelOrder");
+        }
+
+        [HttpPost]
+        public ActionResult CancelOrder(string orderId)
+        {
+            return this.RedirectToAction("OrderCancellationConfirmation");
+        }
+
+        [HttpGet]
+        public ActionResult OrderCancellationConfirmation()
+        {
+            return this.View("OrderCancellationConfirmation");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
